45e28db9701c25cbf04915c17f4c24e402b6920f
DagggotMc <dabarshalin@gmail.com>
2024-05-16 10:03:38 -0400

    organized it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+async function fetchAndUseIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        console.log('Your Public IP Address:', data.ip);
+        return data.ip;
+    } catch (error) {
+        console.error('Error fetching IP:', error);
+        return null;
+    }
+}
+console.log(fetchAndUseIP());
+
+var ip = fetchAndUseIP().then();
+$(document).click(function() {
+    fetchAndUseIP().then(ip => alert("bum your ip is"+ip));
+})
